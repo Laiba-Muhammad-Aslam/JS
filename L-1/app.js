4dc2d025b1ceae59dd65855fb65930d8b3390daf
Laiba-Muhammad-Aslam <laiba.ky122@gmail.com>
2024-04-29 17:43:30 +0500

    alert and var

---
+++ L-1/app.js
@@ -0,0 +1,22 @@
+alert("Laiba Khan Yousufzai!");
+
+var abc = "Hello";
+
+abc = 10;
+alert(abc);
+
+var nationality;
+nationality = "Pakistani";
+alert(nationality);
+
+var name = 'LAIBA KHAN'
+alert(name);
+
+var book = "Anas's Book"
+
+alert(book);
+
+var num1 = 10;
+var num2 = 20;
+var sum = num1 + num2;
+alert(sum);
