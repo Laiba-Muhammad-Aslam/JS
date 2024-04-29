1613575a30389a3c0628dce56203b7aad17e3666
Laiba-Muhammad-Aslam <laiba.ky122@gmail.com>
2024-04-29 22:27:15 +0500

    Practice set chap1

--- Lectures/L-1/app.js
+++ Lectures/L-1/app.js
@@ -20,3 +20,4 @@ var num1 = 10;
 var num2 = 20;
 var sum = num1 + num2;
 alert(sum);
+window.alert(book);
---
+++ Practice/chap1/app.js
@@ -0,0 +1,22 @@
+// CHAP 1 ALERT 
+
+// Q#1
+
+alert("Hello Everyone!!");
+
+// Q#2
+
+alert("Error! Please enter a valid password.");
+
+// Q#3
+
+alert("Welcome to JS Land... Happy Coding!");
+
+// Q#4 
+
+alert("Welcome to JS Land...");
+alert("Happy Coding! Prevent this page from creating additional dialogs.");
+
+// Q#5 
+
+console.log("Hello... I can run JS through my web browser's console");
