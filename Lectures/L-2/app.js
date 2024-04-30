cf54fea5893bc7a6a3298f1acef7ffc6f0a4e965
Laiba-Muhammad-Aslam <laiba.ky122@gmail.com>
2024-04-30 21:06:31 +0500

    math expression

---
+++ Lectures/L-2/app.js
@@ -0,0 +1,19 @@
+var a = "Hello World!";
+
+document.write("<h1>" + a + "</h1>");
+
+alert("10" - 2);
+alert(10%3);
+
+var num = 10;
+alert(num);
+num++;
+alert(num);
+num--;
+alert(num);
+
+// post increment vs pre increment
+
+var n1 = 10;
+alert(++n1);
+alert(n1++);
