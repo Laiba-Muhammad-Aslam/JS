fe13878d32dba8371faf2439a2382035a4fac7e6
Laiba-Muhammad-Aslam <laiba.ky122@gmail.com>
2024-05-01 18:12:56 +0500

    Q#1 of Practice set of chap 5

---
+++ Practice/chap5/app.js
@@ -0,0 +1,8 @@
+// MATH EXPRESSIONS
+
+var num1 = prompt("Enter your first Number: ");
+var num2 = prompt("Enter your second Number: ");
+var sum = (num1) + (num2);
+alert(sum);
+document.write( `sum of ${num1} and ${num2} is ${sum}`);
+
