0e1451ebae1ff3dfa72e32d9a6c13a3cd3359014
Laiba-Muhammad-Aslam <laiba.ky122@gmail.com>
2024-05-01 12:50:17 +0500

    Practice set chap 4

---
+++ Practice/chap4/app.js
@@ -0,0 +1,27 @@
+// VARIABLE NAMES: LEGAL & ILLEGAL
+
+// Q#1
+
+// Q#2
+
+// Legal Variable names
+
+var $name = "Laiba";
+var _age = "19";
+var id = "2120";
+var salary_ = "65000";
+var abc1 = 4232442;
+var universityName = "UBIT UNIVERSITY OF KARACHI";
+
+// ILLEGAL variable name
+
+// var 1 = "Maaz";
+// var alert = "myAlert";
+
+// Q#3
+
+document.write("<h1>Rules for naming JS variables</h1>");
+document.write("<p>Variable names can only contain numbers, $ and _. For example $my_1stVariable</p>");
+document.write("<p>Variables must begin with a $ or _.  For example $name, _name or name</p>");
+document.write("<p>Variable names are case sensitive</p>");
+document.write("<p>Variable names should not be JS keywords</p>");
