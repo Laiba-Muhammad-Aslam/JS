fdd7c38d1e15fcac196df889249ddd7a10fb26c1
Laiba-Muhammad-Aslam <laiba.ky122@gmail.com>
2024-05-04 21:17:23 +0500

    chap11, 12 and chap13

---
+++ Lectures/L-6/app.js
@@ -0,0 +1,18 @@
+
+var flag = true;
+console.log(!!flag);
+
+var studentNmae = "Zain";
+var myMessageForStudentName = "Not endrolled in course "
+
+if(studentNmae === "Zain"){
+    console.log("studentName ",studentName);
+    myMessageForStudentName = studentName;
+}
+
+if(studentNmae === "Usman"){
+    console.log("studentName ",studentName);
+    myMessageForStudentName = studentName;
+}
+
+alert(myMessageForStudentName);
