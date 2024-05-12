61ba1510611b337824e9d21b4986440cbf21f7cb
Laiba-Muhammad-Aslam <laiba.ky122@gmail.com>
2024-05-12 20:12:47 +0500

    question 2 and question 3 of chap 12-13

---
+++ Practice/chap12-13/app.js
@@ -0,0 +1,29 @@
+// QUESTION # 2
+
+// var int1 = +prompt("Enter your first number: ");
+// var int2 = +prompt("Enter your second number: ");
+
+// if(int1 > int2){
+//     // alert(int1 + "is greater");
+//     console.log(int1 + "is greater");
+// }
+// else if(int1 === int2){
+//     // alert(`${int1} and ${int2} are equal`);
+//     console.log(`${int1} and ${int2} are equal`);
+// }else{
+//     // alert(`${int2} is greater`);
+//     console.log(`${int2} is greater`);
+// }
+
+
+// QUESTION # 3
+
+var num = +prompt("Enter your number: ");
+
+if (num > 0){
+    console.log("Number is positive");
+}else if (num == 0){
+    console.log("Number is equal to Zero");
+}else{
+    console.log("Number is negative");
+}
