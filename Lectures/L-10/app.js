7a1abbcb045bcf2436aa2f5ac35777cf4ea0b7a6
Laiba-Muhammad-Aslam <laiba.ky122@gmail.com>
2024-05-16 23:36:35 +0500

    Task

---
+++ Lectures/L-10/app.js
@@ -0,0 +1,11 @@
+// var str = "UBIT";
+// str+= "Karachi";
+// console.log(str);
+
+var str;
+str+= "Karachi";
+console.log(str); //undefined Karachi
+
+var str = "";
+str+= "Karachi";
+console.log(str); //Karachi
