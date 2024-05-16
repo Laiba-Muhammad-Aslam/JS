07b9db4806f02c5936cfb759bb63d6f909b771f5
Laiba-Muhammad-Aslam <laiba.ky122@gmail.com>
2024-05-17 00:16:54 +0500

    missing element in an array

--- Lectures/L-10/app.js
+++ Lectures/L-10/app.js
@@ -4,8 +4,31 @@
 
 var str;
 str+= "Karachi";
-console.log(str); //undefined Karachi
+console.log(str); //undefinedKarachi
 
 var str = "";
 str+= "Karachi";
 console.log(str); //Karachi
+
+var str = null;
+str+= "Karachi";
+console.log(str); //nullKarachi
+
+
+var arr = [11, 13, 15, 18, 20];
+var missing = [];
+// var newarr2 = [];
+
+for(var i= 0; i<arr.length; i++){
+    var diff = arr[i + 1] - arr[i];
+    if (diff > 1 ){
+        for (var j = 1; j < diff; j++){
+            missing.push(arr[i] + j);
+        }
+    }
+}
+console.log(arr);
+console.log(missing);
+// var newarr1 = arr + missing;
+// newarr2.push(newarr1);
+// console.log(typeof newarr2);
