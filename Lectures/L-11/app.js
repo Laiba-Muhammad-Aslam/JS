938e0e59f89634b05e0f36672cf9f60cab165bb9
Laiba-Muhammad-Aslam <laiba.ky122@gmail.com>
2024-05-19 15:38:06 +0500

    practice loop questions

--- Lectures/L-11/app.js
+++ Lectures/L-11/app.js
@@ -18,3 +18,18 @@ for (var i = 0; i < 5; i++){
         console.log(i, j);
     }
 }
+
+// var fullName = "laiba aslam";
+// fullName = fullName.slice(0,1).toUpperCase + fullName.slice(1);
+// console.log(fullName); 
+
+var fullName = "laiba aslam";
+var result = "";
+
+for (var i = 0; i < fullName.length; i++){
+    console.log(fullName[i]);
+    if (i === 0){
+        result += fullName[i].toUpperCase;
+    }
+    console.log(result)
+}
