b31b4031d9810a9f573131b211437e28c3385860
Laiba-Muhammad-Aslam <laiba.ky122@gmail.com>
2024-05-21 11:36:20 +0500

    for loop questions

--- Lectures/L-11/app.js
+++ Lectures/L-11/app.js
@@ -23,19 +23,79 @@ for (var i = 0; i < 5; i++){
 // fullName = fullName.slice(0,1).toUpperCase + fullName.slice(1);
 // console.log(fullName); 
 
+// MY SOLUTION 
+
 var fullName = "laiba aslam";
 var result = "";
+var temp = "";
 
 for (var i = 0; i < fullName.length; i++){
     // console.log(fullName[i]);
     if (i === 0){
         result += fullName[i].toUpperCase();
     }else if (fullName[i] === " "){
-        result += fullName[i + 1].toUpperCase();
+        temp = fullName[i + 1].toUpperCase();
+        // console.log(temp);
         // console.log(result)
     }
     else{
+        console.log(temp);
+        if (temp){
+            result += " " +  temp;
+        }
+        else{
+            result += fullName[i];
+        }
+        temp = "";
+    }
+}
+console.log(result);
+
+// SOLUTION 
+
+// var fullName = "laiba khan yousufzai";
+// var result = "";
+// var temp = "";
+
+// for (var i = 0; i < fullName.length; i++){
+//     if (i === 0){
+//         result += fullName[i].toUpperCase();
+//     }else if (fullName[i] === " "){
+//         temp = fullName[i + 1].toUpperCase();
+//     }
+//     else{
+//         // console.log(temp);
+//         if (temp){
+//             result += " " +  temp;
+//             temp = "";
+//         }
+//         else{
+//             result += fullName[i];
+//         }
+//     }
+// }
+// console.log(result);
+
+
+var fullName = "laiba khan yousufzai";
+var result = "";
+var temp = "";
+
+for (var i = 0; i < fullName.length; i++){
+    if (i === 0){
+        result += fullName[i].toUpperCase();
+    }else if (fullName[i] === " "){
+        temp = fullName[i + 1].toUpperCase();
         result += fullName[i];
+    }
+    else{
+        if (temp){
+            result += temp;
+            temp = "";
+        }
+        else{
+            result += fullName[i];
+        }
     }
 }
 console.log(result);
