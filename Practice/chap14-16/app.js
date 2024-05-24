cc01871719245f58652be15c467288547db7cad6
Laiba-Muhammad-Aslam <laiba.ky122@gmail.com>
2024-05-24 12:06:46 +0500

    question 1- 7 of practice set 14-16

---
+++ Practice/chap14-16/app.js
@@ -0,0 +1,55 @@
+// QUESTION # 1
+
+var students = [];
+
+// QUESTION # 2
+
+var students = {};
+
+// QUESTION # 3
+
+var stringArray = [];
+stringArray[0] = "Laiba";
+stringArray[1] = "Maaz";
+stringArray[2] = "Yush";
+
+// QUESTION # 4
+
+var numbarArray = [];
+numbarArray[0] = 17;
+numbarArray[2] = 177;
+numbarArray[3] = 19;
+numbarArray[4] = 10;
+
+// QUESTION # 5
+
+var booleanArray = [];
+booleanArray[0] = true;
+booleanArray[1] = false;
+booleanArray[2] = false;
+booleanArray[3] = true;
+
+// QUESTION # 6 
+
+var arr = [];
+arr[0] = 100;
+arr[1] = "Laiba Aslam";
+arr[2] = true;
+arr[3] = 4500000;
+
+// QUESTION # 7 
+
+var qualifications = [];
+var qualification;
+qualifications[0] = "SSC";
+qualifications[1] = "HSC";
+qualifications[2] = "BSC";
+qualifications[3] = "BS";
+qualifications[4] = "BCOM";
+qualifications[5] = "MS";
+qualifications[6] = "M.Phil";
+qualifications[7] = "PhD";
+
+for (qualification in qualifications){
+    console.log(qualifications[qualification]);
+}
