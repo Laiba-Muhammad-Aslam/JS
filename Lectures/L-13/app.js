1aaa558cebe04c58bd8c9184251e30703cd9a4a3
Laiba-Muhammad-Aslam <laiba.ky122@gmail.com>
2024-05-31 11:11:05 +0500

    string methods

--- Lectures/L-13/app.js
+++ Lectures/L-13/app.js
@@ -6,3 +6,37 @@ console.log(str.toUpperCase());
 
 var str = "UBIT Karachi";
 console.log(str.toLowerCase());
+
+var str = "UBIT Karachi";
+console.log(str.slice(0,4));
+
+var str = "UBIT Karachi";
+console.log(str.indexOf("Karachi"));
+
+var str = "UBIT Karachi";
+console.log(str.lastIndexOf("a"));
+
+var str = "UBIT Karachi";
+console.log(str.lastIndexOf("x"));
+
+var str = "UBIT Karachi";
+console.log(str.indexOf("x"));
+
+var names = ["Laiba", "Maha", "Muniza"];
+console.log(names.indexOf("Muniza"));
+
+var names = ["Laiba", "Maha", "Muniza"];
+console.log(names.lastIndexOf("Muniza"));
+
+var str = "UBIT Karachi";
+console.log(str.slice(0,1));
+
+// OR 
+
+var str = "UBIT Karachi";
+console.log(str.charAt("U"));
+
+// OR 
+
+var str = "UBIT Karachi";
+console.log(str[0]);
