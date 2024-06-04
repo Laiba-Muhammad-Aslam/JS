993c06149196d25ffce0584a9afd9073face59a5
Laiba-Muhammad-Aslam <laiba.ky122@gmail.com>
2024-06-04 11:25:42 +0500

    Functions and Basic Dom

--- Lectures/L-14/app.js
+++ Lectures/L-14/app.js
@@ -1,50 +1,49 @@
 // Date Object | Functions
 
 var date = new Date(); // Date Object
-console.log(date); 
+console.log(date);
 
-// Changing Date object to String 
+// Changing Date object to String
 
+var date = new Date();
+console.log(date.toString());
 
-var date = new Date(); 
-console.log(date.toString()); 
+// GET DAY
 
-// GET DAY 
-
-var date = new Date(); 
-console.log(date.getDay()); // it returns the index no of day Sunday -- 0 
+var date = new Date();
+console.log(date.getDay()); // it returns the index no of day Sunday -- 0
 
 var days = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
-var date = new Date(); 
+var date = new Date();
 console.log(days[date.getDay()]);
 
-// GET MONTH 
+// GET MONTH
 
-var date = new Date(); 
+var date = new Date();
 console.log(date.getMonth());
 
-var date = new Date(); 
-console.log(date.getDate()); 
+var date = new Date();
+console.log(date.getDate());
 
-var date = new Date(); 
-console.log(date.getFullYear()); 
+var date = new Date();
+console.log(date.getFullYear());
 
-var date = new Date(); 
-console.log(date.getHours()); 
+var date = new Date();
+console.log(date.getHours());
 
-var date = new Date(); 
+var date = new Date();
 console.log(date.getMinutes());
 
-var date = new Date(); 
+var date = new Date();
 console.log(date.getSeconds());
 
-var date = new Date(); 
-console.log(date.getMilliseconds()); 
+var date = new Date();
+console.log(date.getMilliseconds());
 
-var date = new Date(); 
+var date = new Date();
 console.log(date.getTime());
 
-// SET A DATE 
+// SET A DATE
 
 var date = new Date("June 30, 2035");
 console.log(date);
@@ -58,3 +57,43 @@ console.log(date);
 var date = new Date("17-October-2002");
 console.log(date);
 
+// SET A DATE
+
+var date1 = new Date();
+date1.setFullYear(2003);
+console.log(date1);
+
+// FUNCTIONS / METHODS
+
+function abc() {
+  console.log(new Date());
+}
+abc();
+
+function myString(number) {
+  // Parameters
+  console.log(number.toString());
+}
+myString(56); // arguments
+
+// var userInput = +prompt("Enter your Number: ");
+
+// function myString(number){ // Parameters
+//     console.log(number.toString());
+// }
+// myString(userInput); // arguments
+
+// BASIC DOM
+
+console.log(document);
+console.log(document.getElementById("#main"));
+console.log(document.getElementById("#main").innerText);
+console.log(document.getElementById("#main").innerHTML);
+
+document.getElementById("main").innerHTML = "Laiba Khan";
+
+function getValue() {
+  var input = document.getElementById("input").value;
+
+  console.log(input);
+}
