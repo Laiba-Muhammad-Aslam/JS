7baa41b21f78022679519d4888a35dbdffee410e
Laiba-Muhammad-Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2024-06-17 12:09:00 -0700

    Q#3 and Q#4 of chap 21-25

--- Practice/chap21-25/app.js
+++ Practice/chap21-25/app.js
@@ -11,3 +11,14 @@ var model = prompt("Enter your favorite model: ");
 var len = model.length;
 alert(`My favorite phone is: ${model} Lenght of string is: ${len}`);
 
+// QUESTION # 3 
+
+var str = "Pakistani";
+var ind = str.indexOf("n");
+console.log(`String: ${str} Index of "n": ${ind}`);
+
+// QUESTION # 4 
+
+var lastLetter = "Hello World";
+var ind = lastLetter.lastIndexOf("l");
+console.log(`String: ${lastLetter} Last index of "n": ${ind}`);
