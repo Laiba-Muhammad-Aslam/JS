f08e142ae40476b923725b3c894d0e5ef0d2dc0b
Laiba-Muhammad-Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2024-06-16 17:27:42 -0700

    Q#1 and Q#2 of practice set 21-25

---
+++ Practice/chap21-25/app.js
@@ -0,0 +1,13 @@
+// QUESTION # 1 
+
+var firstName = prompt("Enter your First Name: ");
+var secondName = prompt("Enter your Second Name: ");
+var fullName = firstName + " " +  secondName;
+alert("Hello " + fullName);
+
+// QUESTION # 2
+
+var model = prompt("Enter your favorite model: ");
+var len = model.length;
+alert(`My favorite phone is: ${model} Lenght of string is: ${len}`);
+
