56a858f5617ba6f6313b66d4cf94b4c630b9d38d
Laiba-Muhammad-Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2024-06-18 11:41:33 -0700

    Q#5 - Q#10 of chap set 21-25

--- Practice/chap21-25/app.js
+++ Practice/chap21-25/app.js
@@ -22,3 +22,42 @@ console.log(`String: ${str} Index of "n": ${ind}`);
 var lastLetter = "Hello World";
 var ind = lastLetter.lastIndexOf("l");
 console.log(`String: ${lastLetter} Last index of "n": ${ind}`);
+
+// QUESTION # 5
+
+var str = "Pakistani";
+var ind = str.charAt(3);
+console.log(`String: ${str} Character at Index 3: ${ind}`);
+
+// QUESTION # 6
+
+var firstName = prompt("Enter your First Name: ");
+var secondName = prompt("Enter your Second Name: ");
+var fullName = firstName.concat(" " + secondName);
+alert("Hello " + fullName);
+
+// QUESTION # 7 
+
+var city = "Hyderabad";
+var replaceWord = city.replace("Hyder", "Islam");
+console.log(`city ${city} After replacement: ${replaceWord}`);
+
+// QUESTION # 8 
+
+var message = "Ali and Sami are best friends. They play cricket and football together.";
+var replaceMessage = message.replaceAll("and", "&");
+console.log(replaceMessage);
+
+// QUESTION # 9 
+
+var str = "472";
+console.log("Value: " + str + " Type: " + typeof str);
+var num = parseInt(str);
+console.log("Value: " + num + " Type: " + typeof num);
+
+// QUESTION # 10 
+
+var input = prompt("Enter your word");
+var uppercaseWord = input.toUpperCase();
+console.log(`User Input: ${input}. Upper Case: ${uppercaseWord}`);
+
