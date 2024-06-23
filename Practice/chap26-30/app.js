22a41b7b622b4ab0e3d65050ab5e760008e72fea
Laiba-Muhammad-Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2024-06-23 15:08:41 -0700

    Q#1, Q#2 and Q#3 of practice set 26-30

---
+++ Practice/chap26-30/app.js
@@ -0,0 +1,23 @@
+// MATH METHODS
+
+// QUESTION # 1
+
+var userInput = +prompt("Enter a number");
+var round = Math.round(userInput);
+var floorNumber = Math.floor(userInput);
+var ceilNumber = Math.ceil(userInput);
+console.log(`number: ${userInput} round off value: ${round} floor value: ${floorNumber} ceil value: ${ceilNumber}`);
+
+// QUESTION # 2
+
+var userInput = +prompt("Enter a number");
+var round = Math.round(userInput);
+var floorNumber = Math.floor(userInput);
+var ceilNumber = Math.ceil(userInput);
+console.log(`number: ${userInput} round off value: ${round} floor value: ${floorNumber} ceil value: ${ceilNumber}`);
+
+// QUESTION # 3
+
+var num = +prompt("Enter a number");
+var absoluteNumber = Math.abs(num);
+console.log(`The absolute value of ${num} is ${absoluteNumber}`);
