c82e978bff070be7659236898a2defbe05f24cf8
Laiba-Muhammad-Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2024-06-27 21:37:48 -0700

    Q#6 of practice set 6-9

--- Practice/chap6-9/app.js
+++ Practice/chap6-9/app.js
@@ -37,7 +37,7 @@ alert(`Hello ${userName}`);
 // QUESTION # 5
 
 var multiplicationTable = +prompt("Enter a number: ");
-if(multiplicationTable == NaN){
+if(isNaN(multiplicationTable)){
     for(var i = 1; i<=10; i++){
         console.log(`5 * ${i} = ` + 5 * i);
     }
@@ -47,4 +47,48 @@ if(multiplicationTable == NaN){
     }
 }
 
+// QUESTION # 6
 
+var subject1 = prompt("Enter first subject Name: ");
+var obtainedMarks1 = +prompt("Enter first subject Marks: ");
+var subject2 = prompt("Enter second subject Name: ");
+var obtainedMarks2 = +prompt("Enter second subject Marks: ");
+var subject3 = prompt("Enter third subject Name: ");
+var obtainedMarks3 = +prompt("Enter third subject Marks: ");
+
+var totalMarks = 100;
+
+var percentage1 = (obtainedMarks1 * 100) / totalMarks;
+var percentage2 = (obtainedMarks2 * 100) / totalMarks;
+var percentage3 = (obtainedMarks3 * 100) / totalMarks;
+
+var totalObtainedMarks = obtainedMarks1 + obtainedMarks2 + obtainedMarks3;
+var totalPercentage = Math.ceil(((percentage1 + percentage2 + percentage3) * 100) / 300);
+
+var totalObtained = document.getElementById("totalobtained");
+totalObtained.innerText = totalObtainedMarks;
+var totalpercentage = document.getElementById("totalpercentage");
+totalpercentage.innerText = totalPercentage + "%";
+
+var sub1 = document.getElementById("sub1");
+sub1.innerText = subject1;
+
+var sub2 = document.getElementById("sub2");
+sub2.innerText = subject2;
+
+var sub3 = document.getElementById("sub3");
+sub3.innerText = subject3;
+
+var marks1 = document.getElementById("marks1");
+marks1.innerText = obtainedMarks1;
+var marks2 = document.getElementById("marks2");
+marks2.innerText = obtainedMarks2;
+var marks3 = document.getElementById("marks3");
+marks3.innerText = obtainedMarks3;
+
+var p1 = document.getElementById("p1");
+p1.innerText = percentage1 + "%";
+var p2 = document.getElementById("p2");
+p2.innerText = percentage2 + "%";
+var p3 = document.getElementById("p3");
+p3.innerText = percentage3 + "%";
