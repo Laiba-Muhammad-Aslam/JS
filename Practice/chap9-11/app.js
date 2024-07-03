549f339d3827bd24602e516bfa5999391326622a
Laiba-Muhammad-Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2024-07-02 21:48:50 -0700

    Q#4 and Q#5 of practice set 9-11

--- Practice/chap9-11/app.js
+++ Practice/chap9-11/app.js
@@ -31,3 +31,70 @@ if(signalColor === "Red"){
 }else{
     alert("Invalid Siganl color")
 }
+
+// QUESTION # 4
+
+var fuel = +prompt("Enter fuel value in litres");
+if(fuel < 0.25){
+    alert("Please refill the fuel in your car");
+}
+
+// QUESTION # 5 
+
+// PART A 
+
+var a = 4;
+if(++a === 5){
+    alert("Given condition for variable a is true");
+}
+
+// PART B
+
+var b = 82;
+if(b++ === 83){
+    alert("Given condition for variable b is true");
+}
+
+// PART C
+
+var c = 12;
+if(c++ === 13){
+    alert("Given condition for variable c is true");
+}
+
+if(c === 13){
+    alert("Condition 2 is true");
+}
+
+if(++c < 14){
+    alert("Condition 3 is true");
+}
+
+if(c === 14){
+    alert("Condition 4 is true");
+}
+
+// PART D 
+
+var materialCost = 20000;
+var laborCost = 2000;
+var totalCost = materialCost + laborCost;
+if(totalCost === laborCost + materialCost){
+    alert("The cost equals");
+}
+
+// PART E 
+
+if(true){
+    alert("True");
+}
+if(false){
+    alert("False");
+}
+
+// PART F 
+
+if("car" < "cat"){
+    alert("car is smaller than cat");
+}
+
