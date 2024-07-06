338b08160c65f8fbfc17d20e828ad0d44d81a063
Laiba-Muhammad-Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2024-07-05 19:33:59 -0700

    iife

---
+++ OneShots/03_Basics/04_iife.js
@@ -0,0 +1,11 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+
+(function chai(){
+    // named IIFE
+    console.log(`DB Connected`);
+})();
+
+((name) =>{
+    console.log(`DB CONNECTED TWO ${name}`);
+})("laiba");
