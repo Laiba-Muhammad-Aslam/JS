f0df1033678bf5bb5882343155ef818aac5ad33d
Laiba-Muhammad-Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2024-08-27 18:35:59 +0500

    basic validation using js

--- Lectures/L-20/index.js
+++ Lectures/L-20/index.js
@@ -1,3 +1,18 @@
 function validation(event){
     console.log("Test", event);
+    var val = event.target.value;
+    console.log(val);
+
+    if(val){
+        event.target.style.border = '';
+    }else{
+        event.target.style.border = '2px solid red';
+    }
 }
+
+
+function abc(){
+    alert("TTT")
+}
+var main = document.getElementById("main");
+main.innerHTML = '<button onclick= "abc()">Click Now<button>';
