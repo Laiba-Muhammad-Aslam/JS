81be7d6dc0110a3619a840b06b855a129602add6
Laiba-Muhammad-Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2024-08-28 13:00:10 +0500

    set interval and clear interval L-20

--- Lectures/L-20/index.js
+++ Lectures/L-20/index.js
@@ -1,18 +1,39 @@
-function validation(event){
-    console.log("Test", event);
-    var val = event.target.value;
-    console.log(val);
+// function validation(event){
+//     console.log("Test", event);
+//     var val = event.target.value;
+//     console.log(val);
 
-    if(val){
-        event.target.style.border = '';
-    }else{
-        event.target.style.border = '2px solid red';
-    }
-}
+//     if(val){
+//         event.target.style.border = '';
+//     }else{
+//         event.target.style.border = '2px solid red';
+//     }
+// }
 
+
+// function abc(){
+//     alert("TTT")
+// }
+// var main = document.getElementById("main");
+// main.innerHTML = '<button onclick= "abc()">Click Now<button>';
+
+
+
+// function updateBtn(){
+//     var main = document.getElementById("main");
+//     main.className += " bgGreen"
+// }
+
+var count = 0;
 
 function abc(){
-    alert("TTT")
+    var btn = document.getElementById("btn");
+    count+=5;
+    // console.log(count);
+    btn.style.left = count + "px";
 }
-var main = document.getElementById("main");
-main.innerHTML = '<button onclick= "abc()">Click Now<button>';
+var ref = setInterval(abc, 1000);
+
+function stopCar(){
+    clearInterval(ref);
+}
