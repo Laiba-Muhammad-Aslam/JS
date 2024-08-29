1fed48235c5b3a7416406cdd628e8b70e575cdce
Laiba-Muhammad-Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2024-08-29 21:18:17 +0500

    code#2 check for balanced bracket

--- Lectures/L-22/index.js
+++ Lectures/L-22/index.js
@@ -8,3 +8,47 @@ for(var i = 0; i < input.length; i++){
     }
 }
 console.log(arr);
+
+function isBalanced(input){
+    var stack = [];
+
+    for(var i= 0; i < input.length; i++){
+        let x = input[i];
+        if (x == '(' || x == '[' || x == '{')
+        {
+            stack.push(x);
+        }
+        if (stack.length == 0){
+            return false;
+        }
+
+        var check;
+        switch (x){
+        case ')':
+            check = stack.pop();
+            if (check == '{' || check == '[')
+                return false;
+            break;
+
+        case '}':
+            check = stack.pop();
+            if (check == '(' || check == '[')
+                return false;
+            break;
+
+        case ']':
+            check = stack.pop();
+            if (check == '(' || check == '{')
+                return false;
+            break;
+        }
+    }
+    return (stack.length == 0);
+}
+
+var input = "([{}])";
+if (areBracketsBalanced(input))
+    document.write("Balanced ");
+else
+    document.write("Not Balanced ");
+
