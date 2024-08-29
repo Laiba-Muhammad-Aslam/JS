402ab32e7331ae10f1ba518668c596dc40f49b36
Laiba-Muhammad-Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2024-08-29 21:00:34 +0500

    Check for Balanced Brackets

---
+++ Lectures/L-22/index.js
@@ -0,0 +1,10 @@
+var input = "{([])}";
+
+var arr = [];
+
+for(var i = 0; i < input.length; i++){
+    if(input[i] === "{" || input[i] === "(" || input[i] === "["){
+        arr.push(input[i])
+    }
+}
+console.log(arr);
