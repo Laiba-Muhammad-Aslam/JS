eadf5c277e9e087d91124a135f1aa0ea02c65ac8
Laiba Muhammad Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2024-09-07 22:41:01 +0500

    signup and logic feature using local storage, browser

--- Lectures/L-27/index.js
+++ Lectures/L-27/index.js
@@ -31,21 +31,89 @@ function register(){
     email.value = "";
     password.value = "";
 
+    location = "login.html"
     // console.log(name.value , email.value , password.value);
 
 }
+
+// function login(){
+//     var email = document.getElementById("l-email");
+//     var password = document.getElementById("l-password");
+//     var users = getUsers();
+//     var saveUser = {};
+
+//     for(var i = 0; i < users.length; i++){
+//         if(email.value === users[i].email){
+//             saveUser = users[i];
+//             break;
+//         }
+//     }
+//     if(saveUser.email){
+//         if(saveUser.password === password.value){
+//             console.log("User Login successfully")
+//         }else{
+//             console.log("Invalid Password")
+//         }
+//     }else{
+//         console.log("User not Found");
+//     }
+    
+// }
+
+
+
+// ANOTHER WAY TO DO IT 
 
 function login(){
     var email = document.getElementById("l-email");
     var password = document.getElementById("l-password");
     var users = getUsers();
+    var isValid = false;
 
     for(var i = 0; i < users.length; i++){
-        if(users == localStorage.getItem("users")){
-            alert("user Exist");
-        }else{
-            alert("User doesn't exist")
+        if(email.value === users[i].email && password.value === users[i].password){
+            isValid = true;
+            break;
         }
+    }
+    if(isValid){
+            console.log("User Login successfully")
+    }else{
+        console.log("Email or Password is Invalid");
     }
     
 }
+
+
+// CHAP # 76
+
+// window.location.href = "https://www.google.com";
+// location.href = "https://www.google.com";
+
+// var search = new URLSearchParams(window.location.search);
+// search.get("q");
+
+// location.assign("https://www.facebook.com");
+// location.reload();
+// location.hash;
+
+// location.assign --> Yeh next page assign krta hai
+// loaction.replace --> Current history ko replace krdy ga.
+
+
+// HARD RELOAD (Page ko server sy doubara download krky laye ga ismai fast reload hota hai.)
+
+window.location.reload(true) 
+
+// BY DEFAULT FALSE HI HOTA HAI YEH 
+
+window.location.reload(false)  // EMPTY CACHE
+
+// chap 78 
+
+history.back();
+history.forward();
+
+// SKIP MULTIPLE PAGES 
+history.go(-2); // back 2 pages
+history.go(2); //  Go 2 pages forward
