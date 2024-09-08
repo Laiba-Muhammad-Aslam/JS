d693bbac3b967911897ef20e4ea531ace03f7a51
Laiba Muhammad Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2024-09-08 23:56:19 +0500

    learning advance js

---
+++ Lectures/L-29/index.js
@@ -0,0 +1,60 @@
+// Var is function scope 
+
+var a = "Laiba";
+console.log(a);
+
+// let and const are block scope 
+
+if(true){
+    let b = 9;
+}
+// Cannot access outside the block 
+// console.log(b); 
+
+
+// THIS IS POSSIBLE 
+
+const arr = [];
+arr[0] = "XXYZ";
+console.log(arr);
+
+
+const obj = {};
+obj.name = "Laiba";
+console.log(obj);
+
+// THIS IS NOT POSSIBLE 
+
+// const ob = {};
+// ob = {name: "Laiba"};
+// console.log(ob);
+
+
+// HERE COMES THE HOISTING. var hoist hota hai jabky const and let hoist nhi hoty.
+
+// console.log(myName); // Undefined
+// var myName = "Laiba"; 
+
+
+// console.log(myName);
+// let myName = "Laiba";
+
+
+// console.log(myName);
+// const myName = "Laiba";
+
+
+// for(var i = 0; i< 5; i++){
+//     setTimeout(function(){
+//         console.log(i);
+//     }, 1000);
+// }
+
+// IN JS Fetch and Promises have higher  priority than setTimeout, setInterval 
+
+// FOR LOOP RUNS FIRST AS IT IS A SYNCHRONOUS 
+for(var i = 0; i< 5; i++){
+    setTimeout(function(){
+        console.log(i);
+    }, 1000);
+}
