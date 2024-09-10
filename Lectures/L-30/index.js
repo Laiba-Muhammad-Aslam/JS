dc76adad730f1457d14df0aaaba53b4959034028
Laiba Muhammad Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2024-09-10 23:26:46 +0500

    ENHANCED OBJECT LITERALS

--- Lectures/L-30/index.js
+++ Lectures/L-30/index.js
@@ -1,20 +1,46 @@
-let obj = {
-    name: "Laiba",
-    school: {
-        name: "XYZ"
-    }
-};
-let obj1 = obj;
+// let obj = {
+//     name: "Laiba",
+//     school: {
+//         name: "XYZ"
+//     }
+// };
+
+// let obj1 = obj;
 // let obj2 = {...obj1}; // Shallow copy
 // obj2.name = "Maha";
 // HERE WHEN WE DO THIS IT WILL CHANGE IN ALL OBJECT BECAUSE THEY SHARE THE SAME REFRENCE SO IN THIS CASE WE NEED TO CREATE A DEEP COPY INSTEAD OF SHALLOW COPY 
 // obj2.school.name = "ABC"
 
 // DEEP COPY 
-let obj2 = {
-    ...obj1,
-    school: {...obj.school}
-};
-obj2.school.name = "ABC";
-console.log(obj, obj1, obj2);
 
+// INSTEAD OF DOING THIS 
+
+// let obj2 = {
+//     ...obj1,
+//     school: {...obj.school}
+// };
+// obj2.school.name = "ABC";
+// console.log(obj, obj1, obj2);
+
+// DO THIS (BEST WAY IN JS TO CREATE A DEEP COPY)
+
+// let obj2 = JSON.parse(JSON.stringify(obj));
+// obj2.school.name = "ABC";
+// console.log(obj, obj1, obj2);
+
+// ENHANCED OBJECT LITERALS 
+
+let student = "Maaz";
+let school = "SM Public";
+
+// INSTEAD OF DOING THIS 
+// let obj = {
+//     student: student,
+//     school: school
+// }
+
+let obj = {
+    student,
+    school
+}
+console.log(obj);
