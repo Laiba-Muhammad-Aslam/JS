3c3d82ebbfc26e6beb306eded2794f8f00d3d235
Laiba Muhammad Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2024-09-11 12:43:35 +0500

    callback and array functions

--- Lectures/L-30/index.js
+++ Lectures/L-30/index.js
@@ -68,3 +68,84 @@ function dragStart(event){
     console.log(event);
 }
 dragBtn.addEventListener("dragstart", dragStart);
+
+
+// CALLBACK 
+// CALLBACK IS USED FOR ASYNCHRONOUS 
+
+function abc(cb){
+    setTimeout(function(){
+        console.log("Done");
+        cb();
+    }, 5000);
+}
+
+function myCallBack(){
+    console.log("ABC Free hogaya..");
+}
+abc(myCallBack);
+
+// set time out is also a call back function.
+
+
+// ARRAY 
+
+let cities = ["Karachi", "Lahore", "Islamabad"];
+
+// for Each returns nothing it always returns undefined 
+cities.forEach(function(elem, ind, arr){
+    console.log(elem, ind, arr);
+})
+
+// array .map is also a call back function 
+// you can transform an arrray through map function 
+// map returns an array 
+// map iteration plus new array bana raha hai. 
+
+// cities.map(function(elem, ind){
+//     console.log(elem, ind);
+// })
+
+// let result = cities.map(function(elem, ind){
+//     return `${elem}, Pakistan`;
+// });
+// console.log(result);
+
+
+// FILTER
+
+// return true py same array mily ga 
+
+// let result = cities.filter(function(elem, ind){
+//     return true;
+// });
+// console.log(result);
+
+
+// return false py [] mily ga. 
+
+// let result = cities.filter(function(elem, ind){
+//     return true;
+// });
+// console.log(result);
+
+let result = cities.filter(function(elem){
+    return elem === "Lahore";
+});
+console.log(result);
+
+let newarr = [{name: "Karachi", population: 1000}, {name: "Lahore", population: 500}];
+
+let populationResult = newarr.filter(function(elem, i){
+    return elem.population > 500;
+});
+console.log(populationResult);
+
+// ASSIGNMENT 
+
+// A Login page:
+// Dashboard: Displays the number of students we have and their details.
+// Students page: Allows students to add themselves.
+// Attendance page: Includes an input field where, upon entering a student's roll number, their attendance will be marked.
+// A page to view the attendance, showing all present students.
+// Add a feature to extract data from an Excel sheet.
