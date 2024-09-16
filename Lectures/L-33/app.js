abcd0f46cbacca4ace1b5c08505690b6615bb46d
Laiba-Muhammad-Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2024-09-16 23:59:24 +0500

    promises.all()

--- Lectures/L-33/app.js
+++ Lectures/L-33/app.js
@@ -14,23 +14,65 @@
 // data.then(res => console.log(res))
 // .catch(err => console.log("err: " + err));
 
-
-let data = new Promise((resolve, reject) => {
-    fetch('https://fakestoreapi.com/products')
-            .then(res=>res.json())
-            .then(json=> resolve(json))
-            .catch(err => reject(err));
-})
+// WE CAN SHORT THIS CODE  USING ASYN AWAIT 
+// let data = new Promise((resolve, reject) => {
+//     fetch('https://fakestoreapi.com/products')
+//             .then(res=>res.json())
+//             .then(json=> resolve(json))
+//             .catch(err => reject(err));
+// })
 
 // data.then(res => console.log(res))
 // .catch(err => console.log(err));
 
 // SHORT HAND OF THE ABOVE TWO LINES 
 
+// let getData = async () => {
+//     try{
+//         let products = await data;
+//         console.log(products);
+//     }catch(err){
+//         console.error(err);
+//     }
+// }
+// getData();
+
+// SHORT CODE USING ASYN AWAIT 
+
+let getProducts = () => {
+   return new Promise((resolve, reject) => {
+        fetch('https://fakestoreapi.com/products')
+                .then(async (res)=> resolve(await res.json()))
+                .catch(err => reject(err));
+    })
+}
+   
+let getCategories = () => {
+    return new Promise((resolve, reject) => {
+        fetch('https://fakestoreapi.com/products/categories')
+                .then(async (res)=> resolve(await res.json()))
+                .catch(err => reject(err));
+    })
+}
+
+// let getData = async () => {
+//     try{
+//         let products = await getProducts();
+//         console.log(products);
+//     }catch(err){
+//         console.error(err);
+//     }
+// }
+// getData();
+
+// Multiple promises
+
 let getData = async () => {
     try{
-        let products = await data;
-        console.log(products);
+        let promises = [];
+        promises.push(getProducts(), getCategories());
+        let final = await Promise.all(promises);
+        console.log("final ", final);
     }catch(err){
         console.error(err);
     }
