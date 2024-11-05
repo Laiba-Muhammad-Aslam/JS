03f0ac23713aff993de08b2054a80d94f47754fb
Laiba Muhammad Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2024-11-05 18:38:41 +0500

    Question # 2 of chap set 35-38

--- Practice/chap35-38/app.js
+++ Practice/chap35-38/app.js
@@ -5,3 +5,12 @@ function tellTime(){
     console.log(date)
 }
 tellTime();
+
+// QUESTION # 2 
+
+function greet(firstName, lastName){
+    alert("Hello " + firstName + " " + lastName);
+}
+var firstName = prompt("Enter your First Name: ");
+var lastName = prompt("Enter your Last Name: ");
+greet(firstName, lastName);
