6c4079bf858d6c1897418a07ea8884960f19eca8
Laiba Muhammad Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2024-11-06 11:27:04 +0500

    Question # 3 of chap set 35-38

--- Practice/chap35-38/app.js
+++ Practice/chap35-38/app.js
@@ -14,3 +14,12 @@ function greet(firstName, lastName){
 var firstName = prompt("Enter your First Name: ");
 var lastName = prompt("Enter your Last Name: ");
 greet(firstName, lastName);
+
+// QUESTION # 3 
+
+function add(num1, num2){
+    return num1 + num2;
+}
+var num1 = +prompt("Enter First Number: ");
+var num2 = +prompt("Enter Second Number: ");
+alert(add(num1, num2));
