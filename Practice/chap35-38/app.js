f7708f7767c5a9999cd0c1e8248bc6be1e49a60d
Laiba Muhammad Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2024-11-07 11:57:39 +0500

    question # 4 of chap set 35-38

--- Practice/chap35-38/app.js
+++ Practice/chap35-38/app.js
@@ -11,15 +11,42 @@ tellTime();
 function greet(firstName, lastName){
     alert("Hello " + firstName + " " + lastName);
 }
-var firstName = prompt("Enter your First Name: ");
-var lastName = prompt("Enter your Last Name: ");
-greet(firstName, lastName);
+// var firstName = prompt("Enter your First Name: ");
+// var lastName = prompt("Enter your Last Name: ");
+// greet(firstName, lastName);
 
 // QUESTION # 3 
 
 function add(num1, num2){
     return num1 + num2;
 }
+// var num1 = +prompt("Enter First Number: ");
+// var num2 = +prompt("Enter Second Number: ");
+// alert(add(num1, num2));
+
+// QUESTION # 4 
+
+function calculator(num1, numb2, operator){
+    if(operator === "+"){
+        return num1 + num2;
+    }
+    else if(operator === "-"){
+        return num1 - num2;
+    }
+    else if(operator === "*"){
+        return num1 * num2;
+    }
+    else if(operator === "/"){
+        return num1 / num2;
+    }
+    else if(operator === "%"){
+        return num1 % num2;
+    }
+    else{
+        return "Invalid Operation"
+    }
+}
 var num1 = +prompt("Enter First Number: ");
 var num2 = +prompt("Enter Second Number: ");
-alert(add(num1, num2));
+var operator = prompt("Enter Operator: ");
+alert(calculator(num1, num2, operator));
