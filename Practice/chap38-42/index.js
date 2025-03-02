62ba8541d79b13062a680c527ecc7e3cca1eac1c
Laiba-Muhammad-Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2025-03-02 15:40:15 +0500

    Question # 2 of chap set 38-42.

--- Practice/chap38-42/index.js
+++ Practice/chap38-42/index.js
@@ -3,5 +3,23 @@
 function power(a, b){
     return a ** b;
 }
-console.log(power(5, 2));
-console.log(power(3, 2));
+// console.log(power(5, 2));
+// console.log(power(3, 2));
+
+// QUESTION # 2
+
+function leapYear(year){
+    let leap = year % 4;
+    if(leap === 0){
+        console.log(`Year ${year} is a leap Year`);
+    }else{
+        console.log(`Year ${year} is not a leap Year`);
+    }
+}
+leapYear(2012);
+leapYear(1997);
+leapYear(2016);
+leapYear(2020);
+leapYear(2003);
+leapYear(2000);
+leapYear(2004);
