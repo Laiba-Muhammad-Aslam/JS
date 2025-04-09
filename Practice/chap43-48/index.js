b1139389a16206a29dc3748f1531707d8d6d6822
Laiba-Muhammad-Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2025-04-09 11:44:11 +0500

    Question # 2 of chap set 43-48

--- Practice/chap43-48/index.js
+++ Practice/chap43-48/index.js
@@ -1,5 +1,15 @@
+// Question # 1
 let discountLink = document.getElementById("discount");
 function  discountAlert() {
     alert("Congratulations...")
 }
-discountLink.addEventListener("click", discountAlert)
+discountLink.addEventListener("click", discountAlert);
+
+// Question # 2
+let shopNow = document.getElementsByClassName("shopNow");
+for(let i = 0; i < shopNow.length; i++){
+    shopNow[i].addEventListener("click", () => {
+        alert("Thanks for purchasing a phone from us");
+    });
+}
+
