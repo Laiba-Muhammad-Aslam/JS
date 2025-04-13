c06c474701b4332888e25187c9c95baa2574d910
Laiba-Muhammad-Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2025-04-13 16:17:55 +0500

    Question # 5 of chap set 43-48.

--- Practice/chap43-48/index.js
+++ Practice/chap43-48/index.js
@@ -33,3 +33,28 @@ changeImg.addEventListener("mouseover" , ()=> {
 changeImg.addEventListener("mouseout" , ()=> {
     changeImg.src = "assests/iphone.jpg"
 })
+
+// Question # 5 
+
+let counterValue = document.getElementById("counter-value");
+let incrementBtn = document.getElementById("incrementBtn");
+let decrementBtn = document.getElementById("decrementBtn");
+let resetBtn = document.getElementById("resetBtn");
+let count = 0;
+
+function increment(){
+    count++;
+    counterValue.innerText = `Count: ${count}`;
+}
+function decrement(){
+    count--;
+    counterValue.innerText = `Count: ${count}`;
+}
+function reset(){
+    count = 0;
+    counterValue.innerText = `Count: ${count}`;
+}
+
+incrementBtn.addEventListener("click", increment);
+decrementBtn.addEventListener("click", decrement);
+resetBtn.addEventListener("click", reset);
