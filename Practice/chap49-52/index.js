78b71eff56b95b99d677803f82a17bd16180f60a
Laiba-Muhammad-Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2025-04-14 21:13:01 +0500

    Question # 1 of chap set 49-52.

---
+++ Practice/chap49-52/index.js
@@ -0,0 +1,8 @@
+// Question # 1 
+
+function signupForm(){
+    event.preventDefault()
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    console.log(email, password);
+}
