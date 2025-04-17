815665338394a38bedcc891909b20427498aa463
Laiba-Muhammad-Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2025-04-17 20:26:41 +0500

    Question # 2 of chap set 49-52.

--- Practice/chap49-52/index.js
+++ Practice/chap49-52/index.js
@@ -6,3 +6,26 @@ function signupForm(){
     let password = document.getElementById("password").value;
     console.log(email, password);
 }
+
+// Question # 2
+
+let paragraph = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda animi illo saepe voluptates. Ipsam, nostrum asperiores eligendi ea eius atque nesat, eum suscipit? Ratione illo doloribus quo ducimus doloremque numquam maiores ad modi.";
+let lessPara = paragraph.slice(0, 20);
+let isExpanded = false;
+let readMoreBtn = document.getElementById("readMoreBtn");
+
+function readMore(){
+    let content = document.getElementById("readMore");
+    if (!isExpanded) {
+        content.innerHTML = lessPara + '...'; 
+        readMoreBtn.innerHTML = "Read More"; 
+    } else {
+        content.innerHTML = paragraph; 
+        readMoreBtn.innerHTML = "Read Less"; 
+    }
+    
+    isExpanded = !isExpanded; 
+}
+readMore();
+
+readMoreBtn.addEventListener("click", readMore);
