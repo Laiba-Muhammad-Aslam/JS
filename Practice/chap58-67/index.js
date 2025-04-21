b12ccc8e8b7dbbb713e1201d5793a37033d4d8f3
Laiba-Muhammad-Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2025-04-21 10:07:46 +0500

    Part iii of Question 1 of chap set 58-67.

--- Practice/chap58-67/index.js
+++ Practice/chap58-67/index.js
@@ -2,4 +2,14 @@
 
 // Part i 
 let mainContent = document.getElementById("main-content");
+
+// Part ii
 console.log(mainContent);
+
+// Part iii 
+let elementsOfClassRender = document.getElementsByClassName("render");
+// console.log(elementsOfClassRender);
+// Array.from() is used to convert HTML collection into Array.
+Array.from(elementsOfClassRender).forEach(elem => {
+    console.log(elem.innerHTML);
+});
