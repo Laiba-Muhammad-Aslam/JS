d12a286535521cbede349cd1e34b269def543cb6
Laiba-Muhammad-Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2025-06-01 20:30:01 +0500

    Reverse a String.

--- Practice/InterviewPrep/index.js
+++ Practice/InterviewPrep/index.js
@@ -22,3 +22,16 @@ var lengthOfLongestSubstring = function (s) {
 
     return longestStringLength;
 };
+
+// Reverse a String
+
+function reverseString(str){
+    let reverseStr = ""
+
+    for(let i = str.length - 1; i>=0; i--){
+        reverseStr+= str[i]
+        console.log(str[i])
+    }
+    return reverseStr;
+}
+console.log(reverseString("Laiba")) // abiaL
