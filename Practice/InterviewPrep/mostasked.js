5417158b8f707a47bd78e808a458dac4a2358ed1
Laiba-Muhammad-Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2025-06-13 00:14:55 +0500

    Palindrome

--- Practice/InterviewPrep/mostasked.js
+++ Practice/InterviewPrep/mostasked.js
@@ -12,3 +12,9 @@ function findMinMax(arr) {
   return { min: Math.min(...arr), max: Math.max(...arr) };
 }
 
+// 3. Check if Array is Palindrome
+
+function isPalindrome(arr) {
+  return arr.join('') === arr.reverse().join('');
+}
+
