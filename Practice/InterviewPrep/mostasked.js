4ce974a80261a12e08d4efbe303482bfcc278e8b
Laiba Muhammad Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2025-06-27 16:57:16 +0500

    Bubble Sort

--- Practice/InterviewPrep/mostasked.js
+++ Practice/InterviewPrep/mostasked.js
@@ -117,3 +117,13 @@ function binarySearch(arr, target) {
   }
   return -1;
 }
+
+// 17. Bubble Sort
+
+function bubbleSort(arr) {
+  let len = arr.length;
+  for (let i = 0; i < len - 1; i++)
+    for (let j = 0; j < len - 1 - i; j++)
+      if (arr[j] > arr[j + 1]) [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+  return arr;
+}
